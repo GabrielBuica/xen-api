83a8395e8a9943720f0375d39c8a2be75421bf56
Gabriel Buica <danutgabriel.buica@cloud.com>
2024-10-10 10:52:24 +0100

    set originator in XenAPI

--- python3/examples/XenAPI/XenAPI.py
+++ python3/examples/XenAPI/XenAPI.py
@@ -65,16 +65,21 @@ otel = False
 try:
     if os.environ["OTEL_SDK_DISABLED"] == "false":
         from opentelemetry import propagate
-        from opentelemetry.trace.propagation import set_span_in_context, get_current_span
+        from opentelemetry.trace.propagation import (
+            set_span_in_context,
+            get_current_span,
+        )
+
         otel = True
 
 except Exception:
     pass
 
-translation = gettext.translation('xen-xm', fallback = True)
+translation = gettext.translation("xen-xm", fallback=True)
 
-API_VERSION_1_1 = '1.1'
-API_VERSION_1_2 = '1.2'
+API_VERSION_1_1 = "1.1"
+API_VERSION_1_2 = "1.2"
+
 
 class Failure(Exception):
     def __init__(self, details):
@@ -89,23 +94,33 @@ class Failure(Exception):
             return msg
 
     def _details_map(self):
-        return dict([(str(i), self.details[i])
-                     for i in range(len(self.details))])
+        return dict([(str(i), self.details[i]) for i in range(len(self.details))])
 
 
 # Just a "constant" that we use to decide whether to retry the RPC
 _RECONNECT_AND_RETRY = object()
 
+
 class UDSHTTPConnection(httplib.HTTPConnection):
-    """HTTPConnection subclass to allow HTTP over Unix domain sockets. """
+    """HTTPConnection subclass to allow HTTP over Unix domain sockets."""
+
     def connect(self):
         path = self.host.replace("_", "/")
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.connect(path)
 
+
 class UDSTransport(xmlrpclib.Transport):
+    def __init__(self):
+        xmlrpclib.Transport.__init__(self)
+        self.__originator = None
+
+    def set_originator(self, originitor):
+        self.__originator = originitor
+
     def add_extra_header(self, key, value):
-        self._extra_headers += [ (key,value) ]
+        self._extra_headers += [(key, value)]
+
     def with_tracecontext(self):
         if otel:
             headers = {}
@@ -117,9 +132,12 @@ class UDSTransport(xmlrpclib.Transport):
             self._extra_headers = []
             for k, v in headers.items():
                 self.add_extra_header(k, v)
+
     def make_connection(self, host):
         self.with_tracecontext()
 
+        if self.__originator:
+            self.add_extra_header("originator", self.__originator)
         # compatibility with parent xmlrpclib.Transport HTTP/1.1 support
         if self._connection and host == self._connection[0]:
             return self._connection[1]
@@ -127,8 +145,12 @@ class UDSTransport(xmlrpclib.Transport):
         self._connection = host, UDSHTTPConnection(host)
         return self._connection[1]
 
+
 def notimplemented(name, *args, **kwargs):
-    raise NotImplementedError("XMLRPC proxies do not support python magic methods", name, *args, **kwargs)
+    raise NotImplementedError(
+        "XMLRPC proxies do not support python magic methods", name, *args, **kwargs
+    )
+
 
 class Session(xmlrpclib.ServerProxy):
     """A server proxy and session manager for communicating with xapi using
@@ -142,32 +164,41 @@ class Session(xmlrpclib.ServerProxy):
     session.xenapi.session.logout()
     """
 
-    def __init__(self, uri, transport=None, encoding=None, verbose=False,
-                 allow_none=True, ignore_ssl=False):
+    def __init__(
+        self,
+        uri,
+        transport=None,
+        encoding=None,
+        verbose=False,
+        allow_none=True,
+        ignore_ssl=False,
+    ):
 
         verbose = bool(verbose)
         allow_none = bool(allow_none)
 
         if ignore_ssl:
             import ssl
+
             ctx = ssl._create_unverified_context()
-            xmlrpclib.ServerProxy.__init__(self, uri, transport, encoding,
-                                           verbose, allow_none, context=ctx)
+            xmlrpclib.ServerProxy.__init__(
+                self, uri, transport, encoding, verbose, allow_none, context=ctx
+            )
         else:
-            xmlrpclib.ServerProxy.__init__(self, uri, transport, encoding,
-                                           verbose, allow_none)
+            xmlrpclib.ServerProxy.__init__(
+                self, uri, transport, encoding, verbose, allow_none
+            )
         self.transport = transport
         self._session = None
         self.last_login_method = None
         self.last_login_params = None
         self._API_version = API_VERSION_1_1
 
-
     def xenapi_request(self, methodname, params):
-        if methodname.startswith('login'):
+        if methodname.startswith("login"):
             self._login(methodname, params)
             return None
-        elif methodname == 'logout' or methodname == 'session.logout':
+        elif methodname == "logout" or methodname == "session.logout":
             self._logout()
             return None
         else:
@@ -178,22 +209,20 @@ class Session(xmlrpclib.ServerProxy):
                 if result is _RECONNECT_AND_RETRY:
                     retry_count += 1
                     if self.last_login_method:
-                        self._login(self.last_login_method,
-                                    self.last_login_params)
+                        self._login(self.last_login_method, self.last_login_params)
                     else:
-                        raise xmlrpclib.Fault(401, 'You must log in')
+                        raise xmlrpclib.Fault(401, "You must log in")
                 else:
                     return result
             raise xmlrpclib.Fault(
-                500, 'Tried 3 times to get a valid session, but failed')
+                500, "Tried 3 times to get a valid session, but failed"
+            )
 
     def _login(self, method, params):
         try:
-            result = _parse_result(
-                getattr(self, 'session.%s' % method)(*params))
+            result = _parse_result(getattr(self, "session.%s" % method)(*params))
             if result is _RECONNECT_AND_RETRY:
-                raise xmlrpclib.Fault(
-                    500, 'Received SESSION_INVALID when logging in')
+                raise xmlrpclib.Fault(500, "Received SESSION_INVALID when logging in")
             self._session = result
             self.last_login_method = method
             self.last_login_params = params
@@ -205,8 +234,8 @@ class Session(xmlrpclib.ServerProxy):
             self._API_version = None
         except socket.error as e:
             # pytype false positive: there is a socket.errno in both py2 and py3
-            if e.errno == socket.errno.ETIMEDOUT: # pytype: disable=module-attr
-                raise xmlrpclib.Fault(504, 'The connection timed out')
+            if e.errno == socket.errno.ETIMEDOUT:  # pytype: disable=module-attr
+                raise xmlrpclib.Fault(504, "The connection timed out")
             raise e
 
     def _logout(self):
@@ -229,7 +258,7 @@ class Session(xmlrpclib.ServerProxy):
         host = self.xenapi.pool.get_master(pool)
         major = self.xenapi.host.get_API_version_major(host)
         minor = self.xenapi.host.get_API_version_minor(host)
-        return "%s.%s"%(major,minor)
+        return "%s.%s" % (major, minor)
 
     @property
     def API_version(self):
@@ -238,43 +267,45 @@ class Session(xmlrpclib.ServerProxy):
         return self._API_version
 
     def __getattr__(self, name):
-        if name == 'handle':
+        if name == "handle":
             return self._session
-        elif name == 'xenapi':
+        elif name == "xenapi":
             return _Dispatcher(self.xenapi_request, None)
-        elif name.startswith('login') or name.startswith('slave_local'):
+        elif name.startswith("login") or name.startswith("slave_local"):
             return lambda *params: self._login(name, params)
-        elif name == 'logout':
+        elif name == "logout":
             return _Dispatcher(self.xenapi_request, "logout")
-        elif name.startswith('__') and name.endswith('__'):
+        elif name.startswith("__") and name.endswith("__"):
             return lambda *args, **kwargs: notimplemented(name, args, kwargs)
         else:
             return xmlrpclib.ServerProxy.__getattr__(self, name)
 
+
 def xapi_local(originator=None):
     transport = UDSTransport()
     if originator:
-        transport.add_extra_header("originator", originator)
-    return Session("http://_var_lib_xcp_xapi/", transport=UDSTransport())
+        transport.set_originator(originator)
+    return Session("http://_var_lib_xcp_xapi/", transport)
+
 
 def _parse_result(result):
-    if type(result) != dict or 'Status' not in result:
-        raise xmlrpclib.Fault(500, 'Missing Status in response from server' + result)
-    if result['Status'] == 'Success':
-        if 'Value' in result:
-            return result['Value']
+    if type(result) != dict or "Status" not in result:
+        raise xmlrpclib.Fault(500, "Missing Status in response from server" + result)
+    if result["Status"] == "Success":
+        if "Value" in result:
+            return result["Value"]
         else:
-            raise xmlrpclib.Fault(500,
-                                  'Missing Value in response from server')
+            raise xmlrpclib.Fault(500, "Missing Value in response from server")
     else:
-        if 'ErrorDescription' in result:
-            if result['ErrorDescription'][0] == 'SESSION_INVALID':
+        if "ErrorDescription" in result:
+            if result["ErrorDescription"][0] == "SESSION_INVALID":
                 return _RECONNECT_AND_RETRY
             else:
-                raise Failure(result['ErrorDescription'])
+                raise Failure(result["ErrorDescription"])
         else:
             raise xmlrpclib.Fault(
-                500, 'Missing ErrorDescription in response from server')
+                500, "Missing ErrorDescription in response from server"
+            )
 
 
 # Based upon _Method from xmlrpclib.
@@ -285,9 +316,9 @@ class _Dispatcher:
 
     def __repr__(self):
         if self.__name:
-            return '<XenAPI._Dispatcher for %s>' % self.__name
+            return "<XenAPI._Dispatcher for %s>" % self.__name
         else:
-            return '<XenAPI._Dispatcher>'
+            return "<XenAPI._Dispatcher>"
 
     def __getattr__(self, name):
         if self.__name is None:
