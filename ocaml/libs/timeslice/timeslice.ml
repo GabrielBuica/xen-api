6655a63723c8041def7c3f9b1ed6b02e0473e090
Gabriel Buica <danutgabriel.buica@cloud.com>
2025-03-31 09:53:16 +0100

    CP-52716: `timeslice: maybe_yeild`
    Summary of behaviour:
    
    1. Every global timeslice we want to yield and start calculating the
       ideal times for the next epoch. The resulting values are stored in
       `Tgroup.ThreadGroups`. At this step we also reset the time counters.
       We also increment the epoch counter.
    2. Everytime we hit the periodic hook, we update the time running for
       the current thread inside the epoch.
       If the time running has exceeded the expected ideal time, we decide
       if we want to yield or sleep for the remaing time in the current
       timeslice epoch.
       If the time is exceeded, we also reset the counter for the
       thread_last_yield. This would give a good approxiamtion for how long
       the next thread has been running.
    
    The ideal time is calculated pretty straight forward: we look at
    ThreadGroups with more than 1 thread running. We add up the shares,
    updating the new total share, we normalize each share and we used the
    results as weights to get the ideal time for this ThreadGroup. The ideal
    time for the individual thread is the time for the ThreadGroup divided by
    the number of threads tunning inside the respective class.
    
    Signed-off-by: Gabriel Buica <danutgabriel.buica@cloud.com>

--- ocaml/libs/timeslice/timeslice.ml
+++ ocaml/libs/timeslice/timeslice.ml
@@ -38,17 +38,164 @@ let yield_interval = Atomic.make Mtime.Span.zero
 *)
 let last_yield = Atomic.make (Mtime_clock.counter ())
 
+let thread_last_yield = Atomic.make (Mtime_clock.counter ())
+
 let failures = Atomic.make 0
+
+let with_time_counter_now time_counter f =
+  let now = Mtime_clock.counter () in
+  Atomic.set time_counter now ;
+  f ()
+
+module Runtime = struct
+  let epoch_count = Atomic.make 0
+
+  let maybe_thread_yield ~global_slice_period =
+    let open Xapi_stdext_threads.Threadext in
+    let thread_ctx = ThreadRuntimeContext.get () in
+    thread_ctx.tgroup
+    |> Tgroup.ThreadGroup.get_tgroup
+    |> Option.iter (fun (tgroup : Tgroup.ThreadGroup.tgroup) ->
+           let current_epoch = Atomic.get epoch_count in
+           ( if current_epoch <> thread_ctx.tepoch then
+               (* thread remembers that it is about to run in a new epoch *)
+               let () = thread_ctx.time_running <- Mtime.Span.zero in
+               thread_ctx.tepoch <- current_epoch
+             else
+               (* thread remembers how long it is  running  in the current epoch *)
+               let time_running_since_last_yield =
+                 Mtime_clock.count (Atomic.get thread_last_yield)
+               in
+               let time_running =
+                 Mtime.Span.add thread_ctx.time_running
+                   time_running_since_last_yield
+               in
+               thread_ctx.time_running <- time_running
+           ) ;
+
+           let is_to_sleep_or_yield delay_s =
+             if delay_s > 0. then
+               Tgroup.ThreadGroup.with_one_fewer_thread_in_tgroup tgroup
+               @@ fun () ->
+               (*todo: do not sleep if this is the last thread in the tgroup(s) *)
+               if tgroup.tgroup = Tgroup.Group.authenticated_root then
+                 with_time_counter_now thread_last_yield Thread.yield
+               else
+                 with_time_counter_now thread_last_yield (fun () ->
+                     let _ = Xapi_stdext_unix.Unixext.select [] [] [] delay_s in
+                     ()
+                 )
+           in
+
+           (* fair scheduling decision to check if thread time_running has exceeded
+              tgroup-mandated ideal time per thread *)
+           if
+             Clock.Timer.span_is_longer thread_ctx.time_running
+               ~than:tgroup.time_ideal
+           then
+             let since_last_global_slice =
+               Mtime_clock.count (last_yield |> Atomic.get)
+             in
+             let until_next_global_slice =
+               Mtime.Span.abs_diff global_slice_period since_last_global_slice
+             in
+             let thread_delay_s =
+               (* the delay is upperbounded to be no longer than the yield_interval *)
+               if until_next_global_slice > global_slice_period then
+                 Clock.Timer.span_to_s global_slice_period
+               else
+                 Clock.Timer.span_to_s until_next_global_slice
+             in
+
+             is_to_sleep_or_yield thread_delay_s
+       )
+
+  let incr_epoch ~frequency =
+    let epoch = epoch_count |> Atomic.get in
+    if epoch mod frequency = 0 then
+      Atomic.set epoch_count 1
+    else
+      Atomic.incr epoch_count
+
+  let sched_global_slice ~global_slice_period =
+    (*refresh the eopch counter roughly every 10s for timeslices of 10ms*)
+    incr_epoch ~frequency:1024 ;
+
+    (* goal is to recalculate thread.time_ideal for each thread: *)
+    (* 1) fairness: each thread group get the same amount of time inside the slice  *)
+    (* 2) control : each thread group time is then weighted by its tgroup_share     *)
+    (* 3) delegate: later, asynchronously, each thread decides to maybe yield based
+       on its thread group idea of ideal time per thread *)
+    (* delegation via tgroups minimizes the number of synchronous global writes
+       here from O(threads) to O(groups) *)
+    let time_ideal_of_tgroups groups =
+      Tgroup.ThreadGroup.(
+        let group_share_total =
+          groups |> List.fold_left (fun xs x -> x.tgroup_share + xs) 0
+        in
+        groups
+        |> List.iter (fun g ->
+               let group_share_ratio =
+                 match group_share_total with
+                 | 0 ->
+                     0.
+                 | gst ->
+                     (g.tgroup_share |> float_of_int) /. (gst |> float_of_int)
+               in
+               let group_time_ns =
+                 group_share_ratio
+                 *. (global_slice_period |> Mtime.Span.to_float_ns)
+               in
+               let thread_time_ideal =
+                 match g.thread_count |> Atomic.get with
+                 | 0 ->
+                     0.
+                 | gnt ->
+                     group_time_ns /. (gnt |> float_of_int)
+               in
+               g.time_ideal <-
+                 Mtime.Span.of_float_ns @@ thread_time_ideal |> Option.get
+           )
+      )
+    in
+    let tgroups_with_threads =
+      List.fold_left
+        (fun xs x ->
+          if x.Tgroup.ThreadGroup.thread_count |> Atomic.get > 0 then
+            x :: xs
+          else
+            xs
+        )
+        []
+        (Tgroup.ThreadGroup.tgroups ())
+    in
+    (* reserve cpu time only to tgroups that have threads to run at the moment *)
+    tgroups_with_threads |> time_ideal_of_tgroups
+end
 
 let periodic_hook (_ : Gc.Memprof.allocation) =
   let () =
     try
-      if not (am_i_holding_locks ()) then
-        let elapsed = Mtime_clock.count (Atomic.get last_yield) in
-        if Mtime.Span.compare elapsed (Atomic.get yield_interval) > 0 then (
-          let now = Mtime_clock.counter () in
-          Atomic.set last_yield now ; Thread.yield ()
-        )
+      if !Constants.tgroups_enabled && !Constants.runtime_sched then
+        let yield_interval = Atomic.get yield_interval in
+        if not (am_i_holding_locks ()) then
+          let elapsed = Mtime_clock.count (Atomic.get last_yield) in
+          if Clock.Timer.span_is_longer elapsed ~than:yield_interval then
+            with_time_counter_now last_yield (fun () ->
+                with_time_counter_now thread_last_yield (fun () ->
+                    let () =
+                      Runtime.sched_global_slice
+                        ~global_slice_period:yield_interval
+                    in
+                    Thread.yield ()
+                )
+            )
+          else
+            Runtime.maybe_thread_yield ~global_slice_period:yield_interval
+        else if not (am_i_holding_locks ()) then
+          let elapsed = Mtime_clock.count (Atomic.get last_yield) in
+          if Clock.Timer.span_is_longer elapsed ~than:yield_interval then
+            with_time_counter_now last_yield Thread.yield
     with _ ->
       (* It is not safe to raise exceptions here, it'd require changing all code to be safe to asynchronous interrupts/exceptions,
          see https://guillaume.munch.name/software/ocaml/memprof-limits/index.html#isolation
